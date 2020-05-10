f1f4099429ba300b597c7bd2389a7b0fed04ff3d
danial <dan232ml@gmail.com>
2020-05-10 12:39:40 +0430

    add animation to splash=MainActivity

--- app/src/main/java/ir/danial/choopan/tvonlinepersianchannels/MainActivity.kt
+++ app/src/main/java/ir/danial/choopan/tvonlinepersianchannels/MainActivity.kt
@@ -2,11 +2,17 @@ package ir.danial.choopan.tvonlinepersianchannels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        splash_img_show.animation= AnimationUtils.loadAnimation(this@MainActivity,R.anim.splash_anim_img_show)
+        splash_txt_right.animation=AnimationUtils.loadAnimation(this@MainActivity,R.anim.splash_anim_txt_right)
+        splash_txt_left.animation=AnimationUtils.loadAnimation(this@MainActivity,R.anim.splash_anim_txt_left)
     }
 }
