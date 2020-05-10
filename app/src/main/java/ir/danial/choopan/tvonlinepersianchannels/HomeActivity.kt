51cfd5ff484ef7b73762eb9b38494542ee74af07
danial <dan232ml@gmail.com>
2020-05-10 15:33:26 +0430

    add land

--- app/src/main/java/ir/danial/choopan/tvonlinepersianchannels/HomeActivity.kt
+++ app/src/main/java/ir/danial/choopan/tvonlinepersianchannels/HomeActivity.kt
@@ -1,9 +1,12 @@
 package ir.danial.choopan.tvonlinepersianchannels
 
+import android.content.res.Configuration
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.telecom.Conference
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -12,37 +15,58 @@ import ir.danial.choopan.tvonlinepersianchannels.adapters.Adapter_recyclerView_c
 import ir.danial.choopan.tvonlinepersianchannels.model.TVchannle
 import ir.danial.choopan.tvonlinepersianchannels.model.db_adapters.Adapter_tbl_tvChannels
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlin.math.log
 
 class HomeActivity : AppCompatActivity() {
-    var video_url=""
+    var video_url = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val adapter_tbl_tvChannels=Adapter_tbl_tvChannels(this@HomeActivity)
-        if(video_url==""){
-            lbl_choise_one.visibility=View.VISIBLE
+        val adapter_tbl_tvChannels = Adapter_tbl_tvChannels(this@HomeActivity)
+        Log.i("url",video_url)
+        if (video_url == "") {
+            lbl_choise_one.visibility = View.VISIBLE
             videoView_tv.pause()
-        }
-        recyclerView_channles.layoutManager=LinearLayoutManager(this@HomeActivity,LinearLayout.VERTICAL,false)
-        recyclerView_channles.adapter=Adapter_recyclerView_channels(adapter_tbl_tvChannels.get_channels()) {
-            video_url=it.stream_url
-            videoView_tv.setVideoURI(Uri.parse(it.stream_url))
-            progress_tv.visibility=View.VISIBLE
-            lbl_choise_one.visibility=View.GONE
+        } else {
+            videoView_tv.setVideoURI(Uri.parse(video_url))
+            progress_tv.visibility = View.VISIBLE
+            lbl_choise_one.visibility = View.GONE
             videoView_tv.setOnPreparedListener {
-                progress_tv.visibility=View.GONE
+                progress_tv.visibility = View.GONE
             }
             videoView_tv.start()
+        }
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            videoView_tv.setVideoURI(Uri.parse(video_url))
+            progress_tv.visibility = View.VISIBLE
+            lbl_choise_one.visibility = View.GONE
+            videoView_tv.setOnPreparedListener {
+                progress_tv.visibility = View.GONE
+                videoView_tv.start()
+            }
+        } else {
+            recyclerView_channles.layoutManager = LinearLayoutManager(this@HomeActivity)
+            recyclerView_channles.adapter =
+                Adapter_recyclerView_channels(adapter_tbl_tvChannels.get_channels()) {
+                    video_url = it.stream_url
+                    videoView_tv.setVideoURI(Uri.parse(video_url))
+                    progress_tv.visibility = View.VISIBLE
+                    lbl_choise_one.visibility = View.GONE
+                    videoView_tv.setOnPreparedListener {
+                        progress_tv.visibility = View.GONE
+                    }
+                    videoView_tv.start()
+                }
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
         super.onSaveInstanceState(outState, outPersistentState)
-        outState.putString("uri",video_url)
+        outState.putString("uri", video_url)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        video_url= savedInstanceState.getString("uru").toString()
+        video_url = savedInstanceState.getString("uri").toString()
     }
 }
--- app/src/main/java/ir/danial/choopan/tvonlinepersianchannels/SplashActivity.kt
+++ app/src/main/java/ir/danial/choopan/tvonlinepersianchannels/SplashActivity.kt
@@ -1,10 +1,14 @@
 package ir.danial.choopan.tvonlinepersianchannels
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
@@ -12,11 +16,26 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        splash_img_show.animation= AnimationUtils.loadAnimation(this@SplashActivity,R.anim.splash_anim_img_show)
-        splash_txt_right.animation=AnimationUtils.loadAnimation(this@SplashActivity,R.anim.splash_anim_txt_right)
-        splash_txt_left.animation=AnimationUtils.loadAnimation(this@SplashActivity,R.anim.splash_anim_txt_left)
-        Handler().postDelayed({
-            startActivity(Intent(this,HomeActivity::class.java))
-        },2000)
+        splash_img_show.animation =
+            AnimationUtils.loadAnimation(this@SplashActivity, R.anim.splash_anim_img_show)
+        splash_txt_right.animation =
+            AnimationUtils.loadAnimation(this@SplashActivity, R.anim.splash_anim_txt_right)
+        splash_txt_left.animation =
+            AnimationUtils.loadAnimation(this@SplashActivity, R.anim.splash_anim_txt_left)
+        val connectivityManager =
+            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+        if (!isConnected) {
+            AlertDialog.Builder(this@SplashActivity).setTitle("اررور")
+                .setMessage("برای استفاده از نرم افزار اتصال اینترنت لازم است لطفا اینترنت خود رو وصل کنید و دوباره امتحان کنید ")
+                .setPositiveButton("باشه") { alert, wiche ->
+                    finish()
+                }.show()
+        } else {
+            Handler().postDelayed({
+                startActivity(Intent(this, HomeActivity::class.java))
+            }, 2000)
+        }
     }
 }
